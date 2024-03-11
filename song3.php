0affa0a1763b0810516b327f31f83191f5f86319
JORDAN671 <160459143+JORDAN671@users.noreply.github.com>
2024-03-11 20:49:04 +0800

    Add files via upload

---
+++ song3.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="csstyle">
+        <title>At the Cross - Hillsong Worship</title>
+</head>
+<body>
+        <h1>Song - At the Cross - Hillsong Worship</h1>
+
+        <h2>Lyrics:</h2>
+        <?php
+	echo "[Verse 1]\n";
+
+	echo "E/G#     A2           B      C#m7\n";
+	echo "Oh Lord You’ve searched me\n";
+	echo "E/G#      A2     B\n";
+	echo "You know my way\n";
+	echo "E/G#       A2     B    C#m7\n";
+	echo "Even when I fail You\n";
+	echo "A2         Bsus  B\n";
+	echo "I know You love me\n";
+
+
+	echo "[Verse 2]\n";
+
+	echo "E/G#      A2   B    C#m7\n";
+	echo "Your holy presence/n";
+	echo "E/G#    A2       B\n";
+	echo "Surrounding me\n";
+	echo "E/G#   A2     B   C#m7\n";
+	echo "In every season\n";
+	echo "A2         Bsus  B\n";
+	echo "I know You love me\n";
+	echo "A2         Bsus  B\n";
+	echo "I know You love me\n";
+
+
+	echo "[Chorus]\n";
+
+	echo "E                B/D#   C#m\n";
+	echo "At the cross I bow my knee\n";
+	echo "                   E/G#     A\n";
+	echo "Where Your blood was shed for me\n";
+	echo "                           B\n";
+	echo "There’s no greater love than this\n";
+	echo "E              B/D#      C#m\n";
+	echo "You have overcome the grave\n";
+	echo "                     E/G#      A\n";
+	echo "Your glory fills the highest place\n";
+	echo "                  B\n";
+	echo "What can separate me now\n";
+
+
+	echo "[Instrumental]\n";
+
+	echo "[| A    E/G# A | B        | x2]\n";
+
+
+	echo "[Verse 3]\n";
+
+	echo "E/G#     A2   B    C#m7\n";
+	echo "You go before me\n";
+	echo "E/G#      A2       B\n";
+	echo "You shield my way\n";
+	echo "E/G#      A2     B     C#m7\n";
+	echo "Your hand upholds me\n";
+	echo "A2         Bsus  B\n";
+	echo "I know You love me\n";
+
+
+	echo "[Chorus]\n”;
+	echo "E                B/D#   C#m\n”;
+	echo "At the cross I bow my knee\n”;
+	echo "                     E/G#     A\n”;
+	echo "Where Your blood was shed for me\n”;
+	echo "                          B\n”;
+	echo "There’s no greater love than this\n”;
+	echo "E              B/D#      C#m\n”;
+	echo "You have overcome the grave\n”;
+	echo "                  E/G#      A\n”;
+	echo "Your glory fills the highest place\n”;
+	echo "                    B\n”;
+	echo "What can separate me now\n”;
+
+	echo "E                B/D#   C#m\n”;
+	echo "At the cross I bow my knee\n”;
+	echo "                     E/G#     A\n”;
+	echo "Where Your blood was shed for me\n”;
+	echo "                          B\n”;
+	echo "There’s no greater love than this\n”;
+	echo "E              B/D#      C#m\n”;
+	echo "You have overcome the grave\n”;
+	echo "                    E/G#      A\n”;
+	echo "Your glory fills the highest place\n”;
+	echo "                   B\n”;
+	echo "What can separate me now\n”;
+
+
+	echo "[Bridge]\n”;
+
+	echo "              A2   B           C#m7\n”;
+	echo "You tore the veil, You made a way\n”;
+	echo "           A        C#m7  B\n”;
+	echo "When You said that it is done\n”;
+
+	echo "              A2   B           C#m7\n”;
+	echo "You tore the veil, You made a way\n”;
+	echo "           A        C#m7  B\n”;
+	echo "When You said that it is done\n”;
+
+
+	echo "[Verse 4]\n”;
+
+	echo "E/G#     A2       B       C#m7\n”;
+	echo "And when the earth fades\n”;
+	echo "E/G#       A2       B\n”;
+	echo "Falls from my eyes,\n”;
+	echo "E/G#         A2      B    C#m7\n”;
+	echo "And You stand before me\n”;
+	echo "A2         Bsus  B\n”;
+	echo "I know You love me\n”;
+	echo "A2         Bsus  B\n”;
+	echo "I know You love me\n”;
+
+
+	echo "[Chorus]\n”;
+
+	echo "E                B/D#   C#m\n”;
+	echo "At the cross I bow my knee\n”;
+	echo "                     E/G#     A\n”;
+	echo "Where Your blood was shed for me\n”;
+	echo "                            B\n”;
+	echo "There’s no greater love than this\n”;
+	echo "E              B/D#      C#m\n”;
+	echo "You have overcome the grave\n”;
+	echo "                     E/G#      A\n”;
+	echo "Your glory fills the highest place\n”;
+	echo "                    B\n”;
+	echo "What can separate me now\n”;
+
+	echo "E                B/D#   C#m\n”;
+	echo "At the cross I bow my knee\n”;
+	echo "                     E/G#     A\n”;
+	echo "Where Your blood was shed for me\n”;
+	echo "                           B\n”;
+	echo "There’s no greater love than this\n”;
+	echo "E              B/D#      C#m\n”;
+	echo "You have overcome the grave\n”;
+	echo "                     E/G#      A\n”;
+	echo "Your glory fills the highest place\n”;
+	echo"                    B\n”;
+	echo "What can separate me now\n”;
+
+
+	echo "[Bridge]\n"
+
+	echo "              A2   B           C#m7\n”;
+	echo "You tore the veil, You made a way\n”;
+	echo "           A        C#m7  B\n”;
+	echo "When You said that it is done\n”;
+
+	echo "              A2   B           C#m7\n”
+	echo "You tore the veil, You made a way\n”
+	echo "           A        C#m7  B\n”;
+	echo "When You said that it is done\n”;
+
+	echo "              A2   B           C#m7\n”;
+	echo "You tore the veil, You made a way\n”;
+	echo "           A        C#m7  B\n”;
+	echo "When You said that it is done\n”;
+
+	echo "              A2   B           C#m7\n”;
+	echo "You tore the veil, You made a way\n”;
+	echo "           A        C#m7  B\n”;
+	echo "When You said that it is done\n”;
+
+
+	echo "[Outro]\n”;
+
+	echo "[| A    E/G# A | B        |]\n”;
+	echo "[| A    E/G# A | B (hold)]\n”;
+
+	?>
+</body>
+</html>
+
