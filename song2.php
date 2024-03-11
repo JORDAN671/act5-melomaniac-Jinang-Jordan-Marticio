2527b49582309370e8fbff19522fc655be55c677
JORDAN671 <160459143+JORDAN671@users.noreply.github.com>
2024-03-11 20:44:55 +0800

    Add files via upload

---
+++ song2.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="csstyle">
+	<title>Through It All - Hillsong Worship</title>
+</head>
+<body>
+	<h1>Song - Through It All - Hillsong Worship</h1>
+
+	<h2>Lyrics:</h2>
+	<?php
+	echo "G          D/F#       Em\n";
+	echo "You are forever in my life\n";
+	echo "    C                  D\n";
+	echo "You see me through the seasons\n";
+	echo "G     D/F#       Em\n";
+	echo "Cover me with Your hands\n";
+	echo "    C               D\n";
+	echo "And lead me in Your righteousness\n";
+
+	echo "[Refrain]\n";
+	echo "    Bm         C\n";
+	echo "And I look to You\n";
+	echo "   Bm         C D\n";
+	echo "And I wait on You\n";
+
+	echo "[Chorus]\n";
+	echo "     G\n";
+	echo "I’ll sing to you Lord\n";
+	echo "   C\n";
+	echo "A hymn of love\n";
+	echo "       D       Em     C D\n";
+	echo "For your faithfulness to me\n";
+	echo "   G               C\n";
+	echo "I’m carried in everlasting arms\n";
+	echo "      D      G      C;
+	echo "You’ll never let me go\n";
+	echo "D          G  D\n";
+	echo "Through it all\n";
+
+	echo "[Repeat]\n";
+
+	echo "[Bridge]\n";
+	echo "G    Em\n";
+	echo "Hallelujah\n";
+	echo "C    D\n";
+	echo "Hallelujah\n";
+
+	?>
+</body>
+</html>
